7a1efd9315ab0f4bb1972b13c70a34fb80dfce5d
Silvio Traversaro <silvio@traversaro.it>
2025-09-17 13:18:15 +0200

    Fix hip build

--- recipes/hip/build.sh
+++ recipes/hip/build.sh
@@ -17,7 +17,7 @@ pushd clr
 mkdir build
 cd build
 
-export CXXFLAGS="$CXXFLAGS -I$SRC_DIR/clr/opencl/khronos/headers/opencl2.2/"
+export CXXFLAGS="$CXXFLAGS -I$SRC_DIR/clr/opencl/khronos/headers/opencl2.2/CL"
 
 install $SRC_DIR/clr/rocclr/platform/prof_protocol.h $PREFIX/include
 
