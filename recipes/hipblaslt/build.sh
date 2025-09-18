647f04523c575529d203226ddfd662990d0cdeac
Silvio Traversaro <silvio@traversaro.it>
2025-09-18 14:46:27 +0200

    Update roctracer and hipblaslt

--- recipes/hipblaslt/build.sh
+++ recipes/hipblaslt/build.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-cmake -GNinja ${CMAKE_ARGS} -Bbuild -S.
+# A lot of part of the build system hardcode amdclang++ as compiler, let's temporary
+# add a symlink with this name
+ln -sf -- "$CXX" "$PREFIX/bin/amdclang++"
+ln -sf -- "$CC" "$PREFIX/bin/amdclang"
+
+cmake -GNinja ${CMAKE_ARGS} -DAMDGPU_TARGETS=gfx942 -Bbuild -S.
 cmake --build ./build
 cmake --install ./build
+
+# Remove amdclang++ symlink to avoid to ship it
+rm -f -- "$PREFIX/bin/amdclang++"
+rm -f -- "$PREFIX/bin/amdclang"
