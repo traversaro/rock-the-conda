0b848ce79c8d92b760e033474131598031a261c4
Silvio Traversaro <silvio@traversaro.it>
2025-09-22 12:02:06 +0200

    Use consistent targets for rocsovler

--- recipes/rocsolver/build.sh
+++ recipes/rocsolver/build.sh
@@ -4,6 +4,11 @@ export HIP_DEVICE_LIB_PATH=$PREFIX/lib/amdgcn/bitcode
 # Workaround for https://github.com/traversaro/rock-the-conda/issues/3#issuecomment-3314729588
 export ROCM_PATH=$PREFIX
 
-cmake -GNinja ${CMAKE_ARGS} -Bbuild -S.
+# See https://github.com/traversaro/rock-the-conda/issues/3#issuecomment-3317381230
+# gfx908 and gfx90a in particular were removed as workaround for rocblas, if you need them you need to patch Tensile
+export CONDA_FORGE_DEFAULT_ROCM_GPU_TARGETS="gfx942;gfx950;gfx1010;gfx1012;gfx1030;gfx1100;gfx1101;gfx1102;gfx1151;gfx1200;gfx1201"
+
+
+cmake -GNinja -DGPU_TARGETS=${CONDA_FORGE_DEFAULT_ROCM_GPU_TARGETS} ${CMAKE_ARGS} -Bbuild -S.
 cmake --build ./build
 cmake --install ./build
