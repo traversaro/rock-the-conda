aabe9c60930c0f92249240fcf68aac32eb76ed97
Filippo Luca Ferretti <filippoluca.ferretti@outlook.com>
2025-11-12 09:36:03 +0100

    Cleanup build script

--- recipes/composable-kernel/build.sh
+++ recipes/composable-kernel/build.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-export CMAKE_HIP_FLAGS="${CMAKE_HIP_FLAGS:-} -O2 -mcode-object-version=6"
-export CMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS:-} -O2 -mcode-object-version=6 -ftemplate-backtrace-limit=0  -fPIE  -Wno-gnu-line-marker -fbracket-depth=512"
-
 mkdir -p build
 cd build
 
@@ -13,8 +10,6 @@ cmake -GNinja \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_PREFIX_PATH="${PREFIX}" \
     -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-    -DCMAKE_HIP_FLAGS="${CMAKE_HIP_FLAGS}" \
-    -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS}" \
     -DGPU_ARCHS=${CONDA_FORGE_DEFAULT_ROCM_GPU_TARGETS} \
     -DBUILD_DEV=OFF \
     -DBUILD_TESTING=OFF \
