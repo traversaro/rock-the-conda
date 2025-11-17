bc7652dcf5989867924f85c4fc3af240ba44e326
Filippo Luca Ferretti <filippoluca.ferretti@outlook.com>
2025-11-17 11:21:46 +0100

    Add rocm-core and llvm patch

--- recipes/hipify/build.sh
+++ recipes/hipify/build.sh
@@ -3,10 +3,7 @@
 set -xeuo pipefail
 
 # Build hipify-clang using CMake
-cmake -S . -B build -G Ninja \
-    ${CMAKE_ARGS} \
-    -DCMAKE_CXX_STANDARD=17
-
+cmake -S . -B build -G Ninja ${CMAKE_ARGS}
 cmake --build build --parallel "${CPU_COUNT}"
 cmake --install build
 
