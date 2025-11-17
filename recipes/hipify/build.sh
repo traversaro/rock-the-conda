d7e16bf03483309bc9e656e7e1a53e7c969d7a8c
Filippo Luca Ferretti <filippoluca.ferretti@outlook.com>
2025-11-17 12:20:01 +0100

    Remove explicit installation of perl script

--- recipes/hipify/build.sh
+++ recipes/hipify/build.sh
@@ -6,6 +6,3 @@ set -xeuo pipefail
 cmake -S . -B build -G Ninja ${CMAKE_ARGS}
 cmake --build build --parallel "${CPU_COUNT}"
 cmake --install build
-
-# Install the hipify-perl script
-install -D -m 755 bin/hipify-perl "${PREFIX}/bin/hipify-perl"
