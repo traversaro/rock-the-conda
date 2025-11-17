09451cd2dce3201bf38c766084df03fce9647ac4
Filippo Luca Ferretti <filippoluca.ferretti@outlook.com>
2025-11-17 10:12:13 +0100

    Build both Clang and Perl implementation

--- recipes/hipify/build.sh
+++ recipes/hipify/build.sh
@@ -2,5 +2,13 @@
 
 set -xeuo pipefail
 
+# Build hipify-clang using CMake
+cmake -S . -B build -G Ninja \
+    ${CMAKE_ARGS} \
+    -DCMAKE_CXX_STANDARD=17
+
+cmake --build build --parallel "${CPU_COUNT}"
+cmake --install build
+
 # Install the hipify-perl script
 install -D -m 755 bin/hipify-perl "${PREFIX}/bin/hipify-perl"
